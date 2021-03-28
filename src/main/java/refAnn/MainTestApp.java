68d4b1e8fdaa2274d6428bd1b991fc48a11b0153
freizeAV <dzfa94@mail.ru>
2021-03-29 00:01:29 +0700

    2

---
+++ src/main/java/refAnn/MainTestApp.java
@@ -0,0 +1,11 @@
+package refAnn;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class MainTestApp {
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        aTest a=new aTest();
+        B.start(a);
+    }
+}
